3c344ef50ba9292d8d4a283011c044d4e28a16ca
jkjarvis <anuttamanand@gmail.com>
2021-01-13 18:59:39 +0530

    form made with user creation

--- stackearth/main/views.py
+++ stackearth/main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Employee, Address, Team, Role, Attendance, Leave, Loan, Bonus
 from .serializers import employeeSerializer, addressSerializer, roleSerializer, teamSerializer, attendanceSerializer,leaveSerializer,loanSerializer,bonusSerializer
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .models import Employee
 
 
 class Get_employees_List(APIView):
@@ -15,16 +18,34 @@ class Get_employees_List(APIView):
 
 
 def homepage(request):
-    return render(request, 'Plus Admin.html')
+    return render(request, 'main/index.html')
 
 @csrf_exempt
 def showForm(request):
-    return render(request,'employee.html')
+    return render(request,'main/form.html')
 
 @csrf_exempt
 def createEmployee(request):
-    data = request.body
-    print(data)
+    data = json.load(request)
+    name = data['name']
+    age = data['dob']
+    email = data['email']
+    ph_number = data['phone']
+    address = data['address']
+    salary = data['salary']
+    role = data['role']
+    team = data['team']
+
+    password = User.objects.make_random_password(length=10)
+    user = User.objects.create(username=name,email=email)
+    user.set_password(password)
+    user.save()
+
+    Employee.objects.create(name=name,age=age,email=email,phone_number=ph_number,address=address,salary=salary,role=role,team=team)
+
+    return HttpResponse('ok')
+
+
 
 def home(request):
     return render(request, 'main/index.html')
